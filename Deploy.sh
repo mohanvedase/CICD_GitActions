b43257e7d427aab1750b5752d72c79c39410eb0b
Mohan krishna <139565500+mohanvedase@users.noreply.github.com>
2023-11-29 02:55:58 +0530

    Update Deploy.sh

--- Deploy.sh
+++ Deploy.sh
@@ -13,28 +13,32 @@ cd "$repo_path"
 # Pull the latest changes from the staging branch
 git pull origin "$staging_branch"
 
-# Check if there are new commits
-if git rev-list "$staging_branch" ^"$main_branch" --count; then
-    echo "New commits detected in $staging_branch branch."
+# Check if there are uncommitted changes in the staging branch
+if [[ $(git status | grep 'modified:' | wc -l) -ne 0 ]]; then
+  # Install or update dependencies (if needed)
+  sudo apt update
+  sudo apt install -y python3-pip
+  sudo pip install -r requirements.txt
 
-    # Install or update dependencies (if needed)
-    sudo apt update
-    sudo apt install -y python3-pip
-    sudo pip install -r requirements.txt
+  # Optionally, stop the existing application (if running)
+  # ...
 
-    # Optionally, stop the existing application (if running)
-    # ...
+  # Start your application (e.g., Flask app)
+  nohup python app.py &&
+  pytest test_app.py
 
-    # Start your application (e.g., Flask app)
-    nohup python app.py &&
-    pytest test_app.py
+  # Optionally, run additional deployment tasks
 
-    # Optionally, run additional deployment tasks
+  # Stage modified files
+  git add .
 
-    # Push changes to the main branch
-    git push origin "$staging_branch":"$main_branch" --force
+  # Commit staged changes with a descriptive message
+  git commit -m "Code is modified"
 
-    echo "Changes pushed to $main_branch branch."
+  # Push the committed changes to the main branch
+  git push origin main
+
+  echo "Changes pushed to $main_branch branch."
 else
-    echo "No new commits in $staging_branch branch. No action needed."
+  echo "No code changes detected. Skipping deployment."
 fi
