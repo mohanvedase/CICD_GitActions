7c640b50ce8091c23b4cf788668d65a4a1f728b1
Mohan krishna <139565500+mohanvedase@users.noreply.github.com>
2023-11-29 10:31:51 +0530

    Update app.py
    simba added to hello world

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def helloWorld():
-    return 'Hello World'
+    return 'Hello World this is SIMBA'
 
 @app.route('/aboutUs')
 def aboutUs():
